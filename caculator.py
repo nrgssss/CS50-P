0e0b145d4d949904cf6da04cd710c319495a6d3e
Narges <nrgsmyrzayy270@gmail.com>
2026-08-09 21:55:15 +0330

    first python codes

---
+++ caculator.py
@@ -0,0 +1,7 @@
+
+x = float(input("what is x? ")) 
+y = float(input ("what is y? "))
+z =round(x / y , 4)
+print (f"{z: .2f}")
+
+print(round(float(input("what is x?")) / float (input("what is y?"))))
